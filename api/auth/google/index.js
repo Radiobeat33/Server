80d40159190be5a0ca32cd02bea6afe2c97e2288
Steven Williams <Steven30135@gmail.com>
2015-02-20 11:02:35 -0800

    push to deployment

--- api/auth/google/index.js
+++ api/auth/google/index.js
@@ -30,6 +30,6 @@ router.get('/google/callback', passport.authenticate('google'), function (req, r
 router.get('/google/complete', function (req, res){
   var token = jwt.sign({foo:'foobar'}, $config.JWT_SECRET, {expiresInMinutes: 60*5});
   res.cookie(token);
-}
+});
 
 module.exports = router; 
